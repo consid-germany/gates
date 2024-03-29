9f6964797ba78cb440a2cf9b7b4b94d5097b18c3
stschaefers <stefan.schaefers@consid.com>
2024-03-29 17:50:43 +0100

    fix(ui): prettier format of gates.e2e.test.ts

--- ui/tests/gates.e2e.test.ts
+++ ui/tests/gates.e2e.test.ts
@@ -1,23 +1,23 @@
-import { test , expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import createClient from 'openapi-fetch';
 import type { paths } from '$lib/generated/api';
 
 const client = createClient<paths>({
-	baseUrl: "http://localhost:9000/api"
+	baseUrl: 'http://localhost:9000/api'
 });
 
 test.beforeEach(async () => {
-	await client.POST("/gates", {
+	await client.POST('/gates', {
 		body: {
 			group: 'some-group',
 			service: 'some-service',
-			environment: 'some-environment',
+			environment: 'some-environment'
 		}
 	});
 });
 
 test.afterEach(async () => {
-	await client.DELETE("/gates/{group}/{service}/{environment}", {
+	await client.DELETE('/gates/{group}/{service}/{environment}', {
 		params: {
 			path: {
 				group: 'some-group',
@@ -33,23 +33,23 @@ test('should open and close gate', async ({ page }) => {
 	await page.goto('/');
 
 	// when (open group)
-	await page.getByRole("tab", { name: "some-group" }).click();
+	await page.getByRole('tab', { name: 'some-group' }).click();
 
 	// then
-	await expect(page.getByRole("heading", { name: "some-service" })).toBeVisible();
-	await expect(page.getByRole("button", { name: "closed" })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'some-service' })).toBeVisible();
+	await expect(page.getByRole('button', { name: 'closed' })).toBeVisible();
 
 	// when (open gate)
-	await page.getByRole("button", { name: "closed" }).click();
+	await page.getByRole('button', { name: 'closed' }).click();
 
 	// then
-	await expect(page.getByRole("button", { name: "open" })).toBeVisible();
+	await expect(page.getByRole('button', { name: 'open' })).toBeVisible();
 
 	// when (close gate)
-	await page.getByRole("button", { name: "open" }).click();
+	await page.getByRole('button', { name: 'open' }).click();
 
 	// then
-	await expect(page.getByRole("button", { name: "closed" })).toBeVisible();
+	await expect(page.getByRole('button', { name: 'closed' })).toBeVisible();
 });
 
 test('should add and remove comment', async ({ page }) => {
@@ -57,22 +57,22 @@ test('should add and remove comment', async ({ page }) => {
 	await page.goto('/');
 
 	// when (open group)
-	await page.getByRole("tab", { name: "some-group" }).click();
+	await page.getByRole('tab', { name: 'some-group' }).click();
 
 	// then
-	await expect(page.getByRole("heading", { name: "some-service" })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'some-service' })).toBeVisible();
 
 	// when (add comment)
-	await page.getByRole("textbox").fill("Some new test comment.");
-	await page.getByRole("button", { name: "Send message" }).click();
+	await page.getByRole('textbox').fill('Some new test comment.');
+	await page.getByRole('button', { name: 'Send message' }).click();
 
 	// then
-	await expect(page.locator(".gate-comment-message")).toBeVisible();
-	await expect(page.locator(".gate-comment-message")).toHaveText("Some new test comment.");
+	await expect(page.locator('.gate-comment-message')).toBeVisible();
+	await expect(page.locator('.gate-comment-message')).toHaveText('Some new test comment.');
 
 	// when (remove comment)
-	await page.locator(".gate-comment-remove-button").click();
+	await page.locator('.gate-comment-remove-button').click();
 
 	// then
-	await expect(page.locator(".gate-comment-message")).not.toBeInViewport();
+	await expect(page.locator('.gate-comment-message')).not.toBeInViewport();
 });
