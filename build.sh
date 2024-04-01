673a8fbc58e0ef568bf8cf8c3b6746c9045a4ede
stschaefers <stefan.schaefers@consid.com>
2024-04-01 21:11:40 +0200

    feat(release): update release workflow

--- build.sh
+++ build.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-#pushd api
-#cargo lambda build --arm64 --release
-#popd
+pushd api
+cargo lambda build --arm64 --release
+popd
 
-#pushd ui
-#npm ci
-#echo "PUBLIC_API_BASE_URL=/api" > .env
-#npm run build
-#popd
+pushd ui
+npm ci
+echo "PUBLIC_API_BASE_URL=/api" > .env
+npm run build
+popd
 
 pushd action
 npm ci
 npm run build
 popd
 
+pushd cdk
+npm ci
+npm run build
+npm run package
+popd
