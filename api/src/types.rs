ea462ae2be4e1a390c5d0cb50e0bcb021d61d237
stschaefers <stefan.schaefers@consid.com>
2024-04-05 15:05:57 +0200

    chore(api): move active_hours temp. to types

--- api/src/types.rs
+++ api/src/types.rs
@@ -1,13 +1,41 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
-use crate::types::active_hours::ActiveHoursPerWeek;
 
 pub mod representation;
 pub mod use_cases;
 pub mod app_state;
-mod active_hours;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct UtcTime { hour: u8, minute: u8, second: u8}
+
+impl UtcTime {
+    fn _new(hour: u8, minute: u8, second: u8) -> Result<Self, &'static str> {
+        if (0..=23).contains(&hour) && (0..=59).contains(&minute) && (0..=59).contains(&second) {
+            Ok(Self { hour, minute, second })
+        } else {
+            Err("Invalid time format: must have 0 <= hour <= 23, 0 <= minute <= 59 and 0 <= second <= 59")
+        }
+    }
+
+    fn _to_string(&self) -> String {
+        format!("{:02}:{:02}:{:02}Z", self.hour, self.minute, self.second)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ActiveHours { pub start: UtcTime, pub end: UtcTime }
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ActiveHoursPerWeek {
+    pub monday: Option<ActiveHours>,
+    pub tuesday: Option<ActiveHours>,
+    pub wednesday: Option<ActiveHours>,
+    pub thursday: Option<ActiveHours>,
+    pub friday: Option<ActiveHours>,
+    pub saturday: Option<ActiveHours>,
+    pub sunday: Option<ActiveHours>,
+}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Config {
