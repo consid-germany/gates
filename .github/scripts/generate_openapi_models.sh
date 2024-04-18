3e60ac7d200420c6529041209052d9a4296a6c25
omarragab <omar.ragab@consid.com>
2024-04-18 13:46:25 +0200

    feat(15): [WIP] generating open api models using Docker image

--- .github/scripts/generate_openapi_models.sh
+++ .github/scripts/generate_openapi_models.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Uncomment the line below to run the script locally
-#cd ../..
+# cd ../..
 
 # Check if Java is installed
-if ! command -v java &> /dev/null; then
-    echo "Java Development Kit (JDK) is not installed. Please install JDK."
-    # Provide instructions on how to install Java
-    exit 1
-fi
-# Check if openapi-generator-cli is installed
-if ! command -v openapi-generator &> /dev/null; then
-    echo "openapi-generator-cli is not installed. Installing..."
-    npm install @openapitools/openapi-generator-cli -g
-fi
-
-# Ensure openapi.yaml exists
-if [ ! -f "openapi.yaml" ]; then
-    echo "Error: openapi.yaml not found. Please place your OpenAPI specification file in the root directory of your project."
-    exit 1
-fi
+#if ! command -v java &> /dev/null; then
+#    echo "Java Development Kit (JDK) is not installed. Please install JDK."
+#    # Provide instructions on how to install Java
+#    exit 1
+#fi
+## Check if openapi-generator-cli is installed
+#if ! command -v openapi-generator &> /dev/null; then
+#    echo "openapi-generator-cli is not installed. Installing..."
+#    npm install @openapitools/openapi-generator-cli -g
+#fi
+#
+## Ensure openapi.yaml exists
+#if [ ! -f "openapi.yaml" ]; then
+#    echo "Error: openapi.yaml not found. Please place your OpenAPI specification file in the root directory of your project."
+#    exit 1
+#fi
 
 # Rust is selected as the generator
 GENERATOR="rust"
@@ -28,8 +28,13 @@ GENERATOR="rust"
 OUTPUT_DIR="openapi"
 
 # Generate models
-openapi-generator-cli generate -i openapi.yaml -g $GENERATOR -o $OUTPUT_DIR
+#openapi-generator-cli generate -i openapi.yaml -g $GENERATOR -o $OUTPUT_DIR
 
+docker run --rm \
+  -v ${PWD}:/local openapitools/openapi-generator-cli generate \
+  -i /local/openapi.yaml \
+  -g $GENERATOR \
+  -o /local/$OUTPUT_DIR
 
 # Move the openapi folder into the api folder
 mv openapi api/
