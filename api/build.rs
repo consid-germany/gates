1eea5ad1f65413c5de22525f52f9ced432dde182
Christian Pflugradt <110765548+christianpflugradt@users.noreply.github.com>
2024-07-31 12:28:17 +0200

    fix(api): generate openapi models through cargo make

--- api/build.rs
+++
@@ -1,9 +0,0 @@
-use std::process::Command;
-
-fn main() {
-    let status = Command::new("sh")
-        .arg("scripts/generate_openapi_models.sh")
-        .status()
-        .expect("Failed to generate openapi models");
-    assert!(status.success(), "Script execution failed");
-}
