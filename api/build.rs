31935f2397b169e4f0c3e6ca5cb53b0b811ccf6c
Christian Pflugradt <110765548+christianpflugradt@users.noreply.github.com>
2024-07-31 10:52:11 +0200

    fix(api): generate openapi models during cargo build

--- api/build.rs
+++ api/build.rs
@@ -1,15 +1,11 @@
 use std::process::Command;
 
 fn main() {
-    // Specify the path to your script relative to the project root
     let script_path = "scripts/generate_openapi_models.sh";
-
-    // Run the script
     let status = Command::new("sh")
         .arg(script_path)
         .status()
-        .expect("Failed to execute script");
-
+        .expect("Failed to generate openapi models");
     if !status.success() {
         panic!("Script execution failed");
     }
