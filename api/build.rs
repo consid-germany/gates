67629d1b58d6d33d0b79c962ba153961f01ee763
Christian Pflugradt <110765548+christianpflugradt@users.noreply.github.com>
2024-07-31 11:28:48 +0200

    clippy

--- api/build.rs
+++ api/build.rs
@@ -1,9 +1,8 @@
 use std::process::Command;
 
 fn main() {
-    let script_path = "scripts/generate_openapi_models.sh";
     let status = Command::new("sh")
-        .arg(script_path)
+        .arg("scripts/generate_openapi_models.sh")
         .status()
         .expect("Failed to generate openapi models");
     assert!(status.success(), "Script execution failed");
